06307a212b32c7d57d2cfaeb845125234bc39b31
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-09-29 22:44:59 -0300

    refactor: refatora classe axuliar que gera JWT

--- app/backend/src/utils/generateJWT.ts
+++ app/backend/src/utils/generateJWT.ts
@@ -2,16 +2,26 @@ import * as Jwt from 'jsonwebtoken';
 
 class JWT {
   public static secret: string;
+  // private static jwtConfig: Jwt.SignOptions;
 
   constructor() {
     // if (!process.env.JWT_SECRET) throw new Error('JWT secret is missing');
     JWT.secret = process.env.JWT_SECRET || 'jwt_secret';
+    // JWT.jwtConfig = { algorithm: 'HS256', expiresIn: '10d' };
   }
 
-  public static sign(payload: { id: number, role: string }) {
+  public static sign(payload: Jwt.JwtPayload): string {
     const token = Jwt.sign(payload, JWT.secret);
     return token;
   }
+
+  // public static verify(token: string): Jwt.JwtPayload | string {
+  //   try {
+  //     return Jwt.verify(token, JWT.secret) as Jwt.JwtPayload;
+  //   } catch (error) {
+  //     return 'Token invalid';
+  //   }
+  // }
 }
 
 export default JWT;
