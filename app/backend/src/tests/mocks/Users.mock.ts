9018c6a1d3d2b887ff74f38925608bafdb03b7f7
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-10-01 18:05:55 -0300

    test: req 11 - testa requisição POST /login/role (INCOMPLETO)

--- app/backend/src/tests/mocks/Users.mock.ts
+++ app/backend/src/tests/mocks/Users.mock.ts
@@ -7,4 +7,11 @@ const user = {
   // senha: secret_user
 }
 
-export { user }
+const userDB = {  username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', };
+
+const userRole = { id: 2, role: 'user', iat: 1696190082, exp: 1697054082 }
+
+export { user, userRole, userDB }
--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -4,10 +4,12 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { user } from './mocks/Users.mock';
+import { user, userDB, userRole } from './mocks/Users.mock';
 import UserModel from '../database/models/UserModel';
 import * as jwt from 'jsonwebtoken'
 import * as bcrypt from 'bcryptjs'
+import JWT from '../utils/generateJWT';
+import Validations from '../middleware/Validations';
 
 chai.use(chaiHttp);
 
@@ -24,12 +26,15 @@ describe('Teste endpoint LOGIN', function () {
 
   //  <------ Tá dando TIME OUT sei lá pq ----->
 
-  // it('Login bem-sucedido - status 200', async function () {
-  //   const loginBody = { email: 'user@user.com', password: 'secret_user' };
-  //   sinon.stub(UserModel, 'findOne').resolves(user as any);
+  // it('Retorna um token quando Login é bem-sucedido - status 200', async function () {
+  //   const loginBody = { email: "user@user.com", password: "secret_user" };
+  //   const dataDB = UserModel.build(userDB);
+  //   sinon.stub(UserModel, 'findOne').resolves(dataDB as any);
+  //   sinon.stub(JWT, 'sign').returns('token');
+  //   // sinon.stub(Validations, 'autheticateUser').returns();
   //   const { status, body } = await chai.request(app).post('/login').send(loginBody);
   //   expect(status).to.equal(200);
-  //   expect(body).to.have('token');
+  //   expect(body).to.have.key('token');
   // })
 
   it('Login mal-sucedido quando email é inexistente - status 400', async function () {
@@ -66,4 +71,13 @@ describe('Teste endpoint LOGIN', function () {
     expect(status).to.equal(404);
     expect(body).to.deep.equal({ message: 'User not found' });
   })
+
+  // <------ JWT secret is missing sei lá porque? ------->
+
+  // it('Retorna a role do usuário - status 200', async function () {
+  //   sinon.stub(JWT, 'verify').returns(userRole);
+  //   const { status, body } = await chai.request(app).get('/login/role').set('Authorization', 'token');
+  //   expect(status).to.equal(200);
+  //   expect(body).to.deep.equal({ role: 'role' }); // mockar retorno
+  // })
 })
