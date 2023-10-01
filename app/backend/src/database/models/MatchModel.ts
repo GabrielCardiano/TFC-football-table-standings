e420dd7f44d62cfe0556ef54d95a2bf8cc4d7319
Gabriel Cardiano <gabriel.cardiano1@gmail.com>
2023-10-01 20:18:58 -0300

    feat: req 13 - cria migration e model da tabela Matches

---
+++ app/backend/src/database/models/MatchModel.ts
@@ -0,0 +1,77 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+import TeamsModel from './TeamsModel';
+
+class MatchModel extends Model
+  <InferAttributes<MatchModel>, InferCreationAttributes<MatchModel>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGols: number;
+  declare inProgress: boolean;
+}
+
+MatchModel.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: { model: 'teams', key: 'id' },
+  },
+
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: { model: 'teams', key: 'id' },
+  },
+
+  awayTeamGols: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+MatchModel.belongsTo(TeamsModel, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+MatchModel.belongsTo(TeamsModel, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+MatchModel.hasMany(TeamsModel, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+MatchModel.hasMany(TeamsModel, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default MatchModel;
